15916455fcd68c46dd9030d8fd008fb3321a4c2e
myildiz17 <muhammetyildiz@gmail.com>
2021-02-12 16:03:22 -0600

    first commit'

---
+++ main.js
@@ -0,0 +1,16 @@
+let mm, mh, jm, jh, mbmi, jbmi
+mm = 78
+mh = 1.69
+jm = 92
+jh = 1.95
+
+mbmi = mm / mh**2
+jbmi = jm / jh**2
+
+console.log(mbmi, jbmi)
+console.log(`Selam kardes's mehabe "dedim sana" ${mm}`)
+
+console.log("abicim nasilsin \n\ Ben iyiyim \n\ peki")
+console.log(`selam
+kelam
+sana`)
