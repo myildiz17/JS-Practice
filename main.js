c76691533b92ce0274ef8c75daaec43c8bd43d1f
myildiz17 <muhammetyildiz@gmail.com>
2021-02-12 18:21:57 -0600

    Truthy values

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
-let mm, mh, jm, jh, mbmi, jbmi
-mm = 78
-mh = 1.69
-jm = 92
-jh = 1.95
+// let mm, mh, jm, jh, mbmi, jbmi
+// mm = 78
+// mh = 1.69
+// jm = 92
+// jh = 1.95
 
-mbmi = mm / mh**2
-jbmi = jm / jh**2
+// mbmi = mm / mh**2
+// jbmi = jm / jh**2
 
-console.log(mbmi, jbmi)
-console.log(`Selam kardes's mehabe "dedim sana" ${mm}`)
+// console.log(mbmi, jbmi)
+// console.log(`Selam kardes's mehabe "dedim sana" ${mm}`)
 
-console.log("abicim nasilsin \n\ Ben iyiyim \n\ peki")
-console.log(`selam
-kelam
-sana`)
+// console.log("abicim nasilsin \n\ Ben iyiyim \n\ peki")
+// console.log(`selam
+// kelam
+// sana`)
+
+// console.log('1990'+80)
+
+// const year = '1990'
+
+// console.log(typeof parseInt("1990"))
+// console.log(typeof parseFloat("1990"))
+// console.log(typeof Number("1990"))
+// console.log(Number("1990"))
+
+// console.log(2==='2')
+// console.log(2=='2')
+
+// const a =15
+// console.log(a.toString(10))
+// console.log(String(15))
+
+// console.log("I am "+23+"years old")
+// console.log("23 "- "10"- 5)
+
+// console.log("23 "* "10")
+// 230
+
+// console.log(Boolean(0))
+// console.log(Boolean(NaN))
+// console.log(Boolean(""))
+// console.log(Boolean("h"))
+// console.log(Boolean({}))
+// console.log(Boolean([]))
+
+// if ("g") { 
+//   console.log("true");
+// } else {
+//   console.log("falsy");
+// }
+
+prompt("what is your favourite number")
